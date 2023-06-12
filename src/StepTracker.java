8ba5f942c8834ac5618a08f0672e56f108a906d5
alewickiy <lewickiy.a@yandex.ru>
2023-06-12 14:50:04 +0300

    Методы отделены друг от друга для удобства читаемости

--- src/StepTracker.java
+++ src/StepTracker.java
@@ -35,6 +35,7 @@ public class StepTracker {
             }
         }
     }
+
     void printStatistic() {
         int monthId = checkMonth();
         int sumSteps = sumStepsFromMonth(monthId);
@@ -48,12 +49,14 @@ public class StepTracker {
         System.out.printf("За этот месяц вы сожгли %d ккал.\n", converter.convertStepsToKilocalories(sumSteps));
         System.out.printf("Лучшая серия достигнутых целей %d дней подряд\n\n", bestSeries(monthId, goalByStepsPerDay));
     }
+
     private void printDaysAndStepsFromMonth(int monthId) {
         for (int i = 0; i < monthToData[monthId].days.length; i++) {
             System.out.println(i+1 + " day: " + monthToData[monthId].days[i]);
         }
         System.out.print("\n");
     }
+
     private int sumStepsFromMonth(int monthId) {
         int monthStepsCount = 0;
         for (int i = 0; i < monthToData[monthId].days.length; i++) {
@@ -61,6 +64,7 @@ public class StepTracker {
         }
         return monthStepsCount;
     }
+
     private int maxStepsMonth(int monthId) {
         return monthToData[monthId].maxSteps();
     }
@@ -100,6 +104,7 @@ public class StepTracker {
             }
         }
     }
+
     int checkSteps() {
         int steps;
         while (true) {
