fa3ce9d1f64b3da890d4b6aa3c8a2f2c54d2cdc3
alewickiy <lewickiy.a@yandex.ru>
2023-06-12 14:59:30 +0300

    непонятные числа теперь находятся в отдельных константах

--- src/Converter.java
+++ src/Converter.java
@@ -1,9 +1,12 @@
 public class Converter {
+    static final double KILOCALORIES_IN_STEP = 0.05;
+    static final double KM_IN_STEP = 0.00075;
+
     int convertToKm(int steps) {
-        return (steps * 75 / 100) / 1000;
+        return (int) (steps * KM_IN_STEP);
     }
 
     int convertStepsToKilocalories(int steps) {
-        return steps * 50 / 1000;
+        return (int) (steps * KILOCALORIES_IN_STEP);
     }
 }
